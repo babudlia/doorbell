92d29a24e8acee2385399f66bca0fd6e638ec6e8
babudlia <48296845+babudlia@users.noreply.github.com>
2020-03-04 20:46:12 +0100

    Update doorbell_camera.py

--- doorbell_camera.py
+++ doorbell_camera.py
@@ -11,6 +11,32 @@ import time
 known_face_encodings = []
 known_face_metadata = []
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 def save_known_faces():
     with open("known_faces.dat", "wb") as face_data_file:
