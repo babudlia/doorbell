cb70a51e0ced080b73988692e06e48d46afe09a6
babudlia <48296845+babudlia@users.noreply.github.com>
2020-03-04 20:54:44 +0100

    Update withfps.py

--- withfps.py
+++ withfps.py
@@ -38,6 +38,19 @@ def gstreamer_pipeline(
         )
     )
 
+def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=60, flip_method=0):
+    """
+    Return an OpenCV-compatible video source description that uses gstreamer to capture video from the camera on a Jetson Nano
+    """
+    return (
+            f'nvarguscamerasrc ! video/x-raw(memory:NVMM), ' +
+            f'width=(int){capture_width}, height=(int){capture_height}, ' +
+            f'format=(string)NV12, framerate=(fraction){framerate}/1 ! ' +
+            f'nvvidconv flip-method={flip_method} ! ' +
+            f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
+            'videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+            )
+
 def save_known_faces():
     with open("known_faces.dat", "wb") as face_data_file:
         face_data = [known_face_encodings, known_face_metadata]
@@ -139,6 +152,7 @@ def lookup_known_face(face_encoding):
 
 def main_loop():
     if running_on_jetson_nano():
+        video_capture = cv2.VideoCapture(get_jetson_gstreamer_source(), cv2.CAP_GSTREAMER)
         video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
         #video_capture = cv2.VideoCapture("http://192.168.0.101:8080/video")
         #video_capture = cv2.VideoCapture("1.ogv")
