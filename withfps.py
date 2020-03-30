6c4a95cac9a26a865beea51256a3b0f0781ecbc0
babudlia <48296845+babudlia@users.noreply.github.com>
2020-03-30 21:25:42 +0200

    Update withfps.py

--- withfps.py
+++ withfps.py
@@ -263,7 +263,8 @@ def main_loop():
             ret, frame = video_capture.read()
         end = time.time()
         seconds = end - start
-        print("Time taken : {0} seconds".format(seconds))
+        #print("Time taken : {0} seconds".format(seconds))
+        print(seconds)
         fps = num_frames / seconds
         print("Estimated frames per second : {0}".format(fps))
 
